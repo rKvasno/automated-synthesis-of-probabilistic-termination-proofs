539d3013941f9ed3ff71f2ae9a14539f175389d4
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-02-26 16:59:46 +0100

    Changed parsers structure

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,19 +1,21 @@
 use std::char;
 use std::error;
 use std::fmt;
+use std::io::Read;
 use super::alts;
 pub mod default;
 
 #[derive(Debug)]
 struct ParserError{
     line: u32,
+    column: u32,
     message: String
 }
 
 impl error::Error for ParserError{}
 impl fmt::Display for ParserError{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}", self.line, self.message)
+        write!(f, "{}, {}: {}", self.line, self.column, self.message)
     }
 }
 
@@ -21,7 +23,7 @@ pub enum Parser {
     Default
 }
 
-pub fn parse<IStream: Iterator<char>>(parser: Parser, input: IStream)
+pub fn parse<Reader: Read>(parser: Parser, input: Reader)
                                         -> Result<alts::ALTS, ParserError> {
     match parser {
         Default => default::parse(input)
