56635bd78ce7f1bf393ae6d40b603a1e9aa73502
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-02-28 17:44:17 +0100

    Implemented ALTS

--- src/alts.rs
+++ src/alts.rs
@@ -1,4 +1,36 @@
+use core::fmt;
+
+enum Arithmetic {
+    Variable{name: Box<str>},
+    Constant{value: f64},
+    Sum(Box<Arithmetic>, Box<Arithmetic>),
+    Product(Box<Arithmetic>, Box<Arithmetic>)
+}
+
+enum Formula {
+    Disjunction(Box<Formula>),
+    Conjuntion(Box<Formula>),
+    Negation(Box<Formula>),
+    Expression(Box<Arithmetic>)
+}
+
+enum Guard {
+    Conditional(Box<Formula>),
+    Probabilistic{probability: f64},
+    Nondeterministic
+}
+
+enum Location {
+    Exit{invariant: Formula},
+    Instruction{invariant: Formula, next: Box<Location>},
+    Branching{invariant: Formula,
+              guard: Guard,
+              left: Box<Location>,
+              right: Box<Location>}
+}
+
 // Annotated Labeled Transition System
 pub struct ALTS {
+    start: Box<Location>,
 }
 
