93c08bfc76ae9a175a77c69f625a531f34235e7a
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-02-28 17:45:30 +0100

    Added Display implementation for Conditionals

--- src/alts.rs
+++ src/alts.rs
@@ -34,3 +34,25 @@ pub struct ALTS {
     start: Box<Location>,
 }
 
+impl fmt::Display for Arithmetic {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) ->  fmt::Result {
+        match self {
+            Self::Variable { name } => write!(f, "{}", name),
+            Self::Constant { value } => write!(f, "{}", value),
+            Self::Sum(lhs, rhs) => write!(f, "({} + {})", lhs, rhs),
+            Self::Product(lhs, rhs) => write!(f, "({} * {})", lhs, rhs)
+        }
+    }
+}
+
+impl fmt::Display for Formula {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) ->  fmt::Result {
+        match self {
+            Self::Expression( expr ) => write!(f, "{}", expr),
+            Self::Negation( form ) => write!(f, "!({})", form),
+            Self::Conjuntion(lhs, rhs) => write!(f, "({} && {})", lhs, rhs),
+            Self::Disjunction(lhs, rhs) => write!(f, "({} || {})", lhs, rhs)
+        }
+    }
+}
+
