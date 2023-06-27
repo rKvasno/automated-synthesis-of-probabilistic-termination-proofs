d2bb4f68bafea1249aa0f22049645d5b829cdfd2
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-06-27 11:06:55 +0200

    Added Variable type alias

--- src/alts/arithmetic.rs
+++ src/alts/arithmetic.rs
@@ -1,8 +1,10 @@
 use std::fmt;
 
+type Variable = Box<str>;
+
 pub enum Arithmetic {
-    Variable{name: Box<str>},
-    Constant{value: f64},
+    Variable(Variable),
+    Constant(f64),
     Sum(Box<Arithmetic>, Box<Arithmetic>),
     Product(Box<Arithmetic>, Box<Arithmetic>)
 }
@@ -10,8 +12,8 @@ pub enum Arithmetic {
 impl fmt::Display for Arithmetic {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) ->  fmt::Result {
         match self {
-            Self::Variable { name } => write!(f, "{}", name),
-            Self::Constant { value } => write!(f, "{}", value),
+            Self::Variable(name) => write!(f, "{}", name),
+            Self::Constant(value) => write!(f, "{}", value),
             Self::Sum(lhs, rhs) => write!(f, "({} + {})", lhs, rhs),
             Self::Product(lhs, rhs) => write!(f, "({} * {})", lhs, rhs)
         }
