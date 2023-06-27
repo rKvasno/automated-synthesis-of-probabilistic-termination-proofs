0195b209e0ad884364f97523c0ebd37cf505757a
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-06-27 11:07:36 +0200

    Extended logical formulas

--- src/alts/formula.rs
+++ src/alts/formula.rs
@@ -5,7 +5,8 @@ pub enum Formula {
     Disjunction(Box<Formula>, Box<Formula>),
     Conjuntion(Box<Formula>, Box<Formula>),
     Negation(Box<Formula>),
-    Expression(Box<Arithmetic>)
+    Expression(Box<Arithmetic>),
+    Constant(bool)
 }
 
 impl fmt::Display for Formula {
@@ -14,7 +15,8 @@ impl fmt::Display for Formula {
             Self::Expression( expr ) => write!(f, "{}", expr),
             Self::Negation( form ) => write!(f, "!({})", form),
             Self::Conjuntion(lhs, rhs) => write!(f, "({} && {})", lhs, rhs),
-            Self::Disjunction(lhs, rhs) => write!(f, "({} || {})", lhs, rhs)
+            Self::Disjunction(lhs, rhs) => write!(f, "({} || {})", lhs, rhs),
+            Self::Constant(value) => write!(f, "{}", value)
         }
     }
 }
