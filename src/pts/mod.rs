7d59621bd5fcc5e4aaa1e4e905d2b21e403f40c5
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-08-01 09:12:57 +0200

    Added basic functions for simple structs

--- src/pts/mod.rs
+++ src/pts/mod.rs
@@ -3,27 +3,58 @@ mod location;
 mod transition;
 
 use crate::pts::location::{Location, LocationHandle};
-use std::{cell::RefCell, borrow::Borrow};
+use std::cell::RefCell;
 
-pub struct Variable(Box<str>);
-pub struct LinearPolynomial(Vec<f64>);
+pub struct Variable {
+    name: Box<str>
+}
+
+pub struct LinearPolynomial {
+    coefficients: Vec<f64>
+}
 
 // linear search in Vec is faster than logarithmic for small number of elements,
 // mostly due to caching
-pub struct VariableMap(Vec<Variable>);
+pub struct VariableMap {
+    variables: Vec<Variable>
+}
 
 #[repr(align(32))] // 32 bytes
 pub struct PTS<'a> {
     locations: Vec<RefCell<Location<'a>>>,
     variables: VariableMap,
-    initial: LocationHandle<'a>,
+    initial: Option<LocationHandle<'a>>,
 }
 
+impl<'a> PTS<'a> {
+    fn new() -> Self {
+        PTS { locations: vec!(), variables: VariableMap::new(), initial: None}
+    }
+}
+
+impl Variable {
+    fn new(name: &str) -> Self {
+        Variable{ name: name.to_string().into_boxed_str() }
+    }
+}
+
+impl LinearPolynomial {
+    fn new() -> Self {
+        LinearPolynomial { coefficients: vec!() }
+    }
+}
+
+
 impl VariableMap {
+
+    fn new() -> Self {
+        VariableMap { variables: vec!() }
+    }
+
     // Return the variable with specified index in a polynomial
     fn index_to_variable(&self, index: usize) -> Option<&str> {
-        if index - 1 < self.0.len() {
-            Some(&self.0[index - 1].0)
+        if index - 1 < self.variables.len() {
+            Some(&self.variables[index - 1].name)
         }
         else {
             None
@@ -31,11 +62,12 @@ impl VariableMap {
     }
 
     // Find variable and return its index, if not found, add it
-    fn find_or_add(&mut self, variable: &str) -> usize {
-        let index = self.0.iter().enumerate().find(|x| x.1.0.as_ref() == variable).map(|x| x.0 + 1);
+    fn find_or_add(&mut self, var: &str) -> usize {
+        let index = self.variables.iter().enumerate().find(|x| x.1.name.as_ref() == var).map(|x| x.0 + 1);
         if index.is_none() {
-            self.0.push(Variable(variable.to_string().into_boxed_str()));
-            self.0.len()
+            self.variables.push(Variable::new(var));
+            // -1 because its after push, +1 because the polynomial indexing is shifted
+            self.variables.len() - 1 + 1
         }
         else {
             index.unwrap()
@@ -45,7 +77,7 @@ impl VariableMap {
 
     // Find variable and return its index, if not found, return None 
     fn variable_to_index(&self, variable: &str) -> Option<usize> {
-        self.0.iter().enumerate().find(|x| x.1.0.as_ref() == variable).map(|x| x.0 + 1)
+        self.variables.iter().enumerate().find(|x| x.1.name.as_ref() == variable).map(|x| x.0 + 1)
 
     }
 }
