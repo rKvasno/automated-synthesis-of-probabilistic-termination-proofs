eb0de8d6a381e951e7b754305d3c6fea92d91922
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-08-04 16:55:07 +0200

    Added constant expression parser

--- src/parsers/default.rs
+++ src/parsers/default.rs
@@ -50,11 +50,37 @@ fn parse_operation<'a>(parse: Pair<'a, Rule>) -> Operation {
     }
 }
 
+// assumes the parses rule is Rule::constant_expr
+fn parse_constant_expr<'a>(parse: Pair<'a, Rule>) -> Constant {
+    
+    //TODO: doesn't do operation priority when chaining multiplicative and additive operations
+    let mut iter = parse.into_inner().into_iter();
+    let first = iter.next().unwrap();
+    let mut acc = match first.as_rule() {
+        Rule::constant => parse_constant(first),
+        Rule::constant_expr => parse_constant_expr(first),
+        _ => panic!(invariant_error!()),
+    };
+
+    loop {
+        let op = iter.next();
+        if op.is_none() { 
+            return acc;
+        }
+        match parse_operation(op.unwrap()) {
+            Operation::Addition => acc += parse_constant_expr(iter.next().unwrap()),
+            Operation::Subtraction => acc -= parse_constant_expr(iter.next().unwrap()),
+            Operation::Multiplication => acc *= parse_constant_expr(iter.next().unwrap()),
+            Operation::Division => acc /= parse_constant_expr(iter.next().unwrap()),
+            Operation::Power => acc = acc.powf(parse_constant_expr(iter.next().unwrap())),
+        }
+    }
+    // unreachable!();
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::parsers::default::parse_operation;
-
-    use super::{DefaultParser, Rule, Parser, parse_variable, parse_constant, Operation};
+    use super::{DefaultParser, Rule, Parser, parse_variable, parse_constant, Operation, parse_operation, parse_constant_expr};
     use std::iter::zip;
 
     #[test]
@@ -84,6 +110,13 @@ mod tests {
             assert_eq!(parse_operation(parse.next().unwrap()), op);
             assert!(parse.next().is_none());
         }
+    }
+
+    #[test]
+    fn constant_expr_sanity() {
+        let mut parse = DefaultParser::parse(Rule::constant_expr, "((4^2 + 5) - (2 * 2 / 2))").unwrap();
+        assert_eq!(parse_constant_expr(parse.next().unwrap()), 19.0);
+        assert!(parse.next().is_none());
     }
 }
 
