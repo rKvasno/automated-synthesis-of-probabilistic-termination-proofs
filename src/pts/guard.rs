0b6f03ad844e8a81dcfab3a381ac29a9c50d9bba
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-08-16 12:14:17 +0200

    Cleaned up Guards

--- src/pts/guard.rs
+++ src/pts/guard.rs
@@ -1,14 +1,16 @@
-use crate::pts::{transition, inequality};
+use crate::pts::{transition, inequality, linear_polynomial};
 use transition::Transition;
 use inequality::InequalitySystem;
 
-pub type Odds = u64;
+use linear_polynomial::constant::Constant;
+
+pub type Probability = Constant;
 
 // 32 bytes
 #[derive(Debug)]
 pub enum Guards<'a>{
     Logic(Vec<(InequalitySystem, Transition<'a>)>),
-    Probabilistic(Vec<(Odds, Transition<'a>)>),
+    Probabilistic(Vec<(Probability, Transition<'a>)>),
     Nondeterministic(Vec<Transition<'a>>),
     Unguarded(Box<Transition<'a>>)
 }
@@ -18,40 +20,3 @@ impl <'a> Default for Guards<'a> {
         Guards::Unguarded(Box::new(Transition::default()))
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use std::mem;
-
-    use super::{Odds, Guards};
-    
-    #[test]
-    fn align_odds() {
-        if mem::size_of::<usize>() == 8{
-            assert_eq!(mem::align_of::<Odds>(), 8);
-        }
-    }
-
-    #[test]
-    fn size_odds() {
-        if mem::size_of::<usize>() == 8{
-            assert_eq!(mem::size_of::<Odds>(), 8);
-        }
-    }
-
-    #[test]
-    fn align_guards() {
-        if mem::size_of::<usize>() == 8{
-            assert_eq!(mem::align_of::<Guards>(), 8);
-        }
-    }
-
-    #[test]
-    fn size_guards() {
-        if mem::size_of::<usize>() == 8{
-            assert_eq!(mem::size_of::<Guards>(), 32);
-        }
-    }
-    
-}
-
