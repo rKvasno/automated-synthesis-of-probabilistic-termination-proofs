8e2ec154c39ffded1dc3a42091f24bb2f967fbff
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-09-09 15:37:02 +0200

    Added GuardsIterator and GuardedTransition

--- src/pts/guard.rs
+++ src/pts/guard.rs
@@ -2,9 +2,10 @@ use std::fmt;
 
 use crate::pts::{inequality, linear_polynomial, transition};
 use inequality::InequalitySystem;
+use linear_polynomial::constant::Constant;
 use transition::Transition;
 
-use linear_polynomial::constant::Constant;
+use super::variable_map::VariableMap;
 
 pub type Probability = Constant;
 
@@ -26,6 +27,10 @@ impl Guards {
             Guards::Nondeterministic(data) => data.is_empty(),
             Guards::Unguarded(_) => false,
         }
+    }
+
+    pub fn iter(&self) -> GuardsIterator {
+        GuardsIterator(self, 0)
     }
 }
 
@@ -50,5 +55,50 @@ impl fmt::Display for GuardsError {
                 "The terminating location can't have outgoing transitions!"
             ),
         }
+    }
+}
+
+pub enum GuardedTransition<'a> {
+    Logic(&'a (InequalitySystem, Transition)),
+    Probabilistic(&'a (Probability, Transition)),
+    Nondeterministic(&'a Transition),
+    Unguarded(&'a Transition),
+}
+
+impl<'a> GuardedTransition<'a> {
+    pub fn as_transition(&'a self) -> &'a Transition {
+        match self {
+            Self::Logic((_, t)) => t,
+            Self::Probabilistic((_, t)) => t,
+            Self::Nondeterministic(t) => t,
+            Self::Unguarded(t) => t,
+        }
+    }
+}
+pub struct GuardsIterator<'a>(&'a Guards, usize);
+
+impl<'a> Iterator for GuardsIterator<'a> {
+    type Item = GuardedTransition<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = match self.0 {
+            &Guards::Logic(ref branches) => {
+                branches.get(self.1).map(|x| GuardedTransition::Logic(x))
+            }
+            &Guards::Probabilistic(ref branches) => branches
+                .get(self.1)
+                .map(|x| GuardedTransition::Probabilistic(x)),
+            &Guards::Nondeterministic(ref branches) => branches
+                .get(self.1)
+                .map(|x| GuardedTransition::Nondeterministic(x)),
+            &Guards::Unguarded(ref boxed_transition) => {
+                if self.1 == 0 {
+                    Some(GuardedTransition::Unguarded(boxed_transition))
+                } else {
+                    None
+                }
+            }
+        };
+        self.1 += 1;
+        item
     }
 }
