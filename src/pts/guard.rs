823c24c6e8f2e59f7995e271f443623cef4f0a00
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-09-16 12:51:47 +0200

    Added DisplayLabel for GuardedTransition

--- src/pts/guard.rs
+++ src/pts/guard.rs
@@ -5,6 +5,8 @@ use inequality::InequalitySystem;
 use linear_polynomial::constant::Constant;
 use transition::Transition;
 
+use super::{variable_map::VariableMap, DisplayLabel};
+
 pub type Probability = Constant;
 
 // 32 bytes
@@ -73,6 +75,23 @@ impl<'a> GuardedTransition<'a> {
         }
     }
 }
+
+impl<'a> DisplayLabel for GuardedTransition<'a> {
+    fn label(&self, variable_map: &VariableMap) -> String {
+        let mut label = match self {
+            Self::Logic((condition, _)) => condition.label(variable_map),
+            Self::Probabilistic((probability, _)) => format!("{}", probability),
+            Self::Nondeterministic(_) => String::default(),
+            Self::Unguarded(_) => String::default(),
+        };
+        for assign in self.as_transition().assignments.iter() {
+            label.push_str("\n");
+            label.push_str(assign.label(variable_map).as_str());
+        }
+        label
+    }
+}
+
 pub struct GuardsIterator<'a>(&'a Guards, usize);
 
 impl<'a> Iterator for GuardsIterator<'a> {
@@ -98,5 +117,137 @@ impl<'a> Iterator for GuardsIterator<'a> {
         };
         self.1 += 1;
         item
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pts::{
+        guard::GuardedTransition,
+        inequality::{Inequality, InequalitySystem},
+        linear_polynomial::{constant::Constant, LinearPolynomial},
+        transition::{Assignment, Transition},
+        variable_map::{Variable, VariableMap},
+        DisplayLabel,
+    };
+
+    #[test]
+    fn logic_label() {
+        let data = (
+            InequalitySystem::mock(vec![Inequality::mock(
+                true,
+                LinearPolynomial::mock(vec![Constant(1.0)]),
+            )]),
+            Transition {
+                assignments: Vec::default(),
+                target: None,
+            },
+        );
+        let guarded_transition = GuardedTransition::Logic(&data);
+        let map = VariableMap::mock(Default::default());
+        assert_eq!(guarded_transition.label(&map), "0 < -1");
+
+        let data = (
+            InequalitySystem::mock(vec![
+                Inequality::mock(
+                    false,
+                    LinearPolynomial::mock(vec![Constant(0.0), Constant(1.0)]),
+                ),
+                Inequality::mock(
+                    true,
+                    LinearPolynomial::mock(vec![Constant(1.0), Constant(2.0), Constant(1.0)]),
+                    // 1 + 2a + len > 0
+                    // 2a + len > -1
+                ),
+            ]),
+            Transition {
+                assignments: vec![Assignment(Variable::new("a"), LinearPolynomial::default())],
+                target: None,
+            },
+        );
+        let guarded_transition = GuardedTransition::Logic(&data);
+        let map = VariableMap::mock(vec![Variable::new("a"), Variable::new("len")]);
+        assert_eq!(
+            guarded_transition.label(&map),
+            "a <= 0\n2a + len < -1\na = 0"
+        );
+    }
+
+    #[test]
+    fn prob_label() {
+        let data = (
+            Constant(0.0),
+            Transition {
+                assignments: vec![
+                    Assignment(
+                        Variable::new("a"),
+                        LinearPolynomial::mock(vec![Constant(0.0), Constant(0.0)]),
+                    ),
+                    Assignment(
+                        Variable::new("b"),
+                        LinearPolynomial::mock(vec![
+                            Constant(2.0),
+                            Constant(1.0),
+                            Constant(2.0),
+                            Constant(0.0),
+                        ]),
+                    ),
+                    Assignment(
+                        Variable::new("a"),
+                        LinearPolynomial::mock(vec![
+                            Constant(-4.0),
+                            Constant(0.0),
+                            Constant(0.0),
+                            Constant(1.0),
+                        ]),
+                    ),
+                ],
+
+                target: None,
+            },
+        );
+        let guarded_transition = GuardedTransition::Probabilistic(&data);
+        let map = VariableMap::mock(vec![
+            Variable::new("a"),
+            Variable::new("b"),
+            Variable::new("c"),
+        ]);
+        assert_eq!(
+            guarded_transition.label(&map),
+            "0\na = 0\nb = a + 2b + 2\na = c - 4"
+        );
+
+        let data = (
+            Constant(0.2222),
+            Transition {
+                assignments: Vec::default(),
+                target: None,
+            },
+        );
+        let guarded_transition = GuardedTransition::Probabilistic(&data);
+        let map = VariableMap::mock(Default::default());
+        assert_eq!(guarded_transition.label(&map), "0.2222");
+    }
+
+    #[test]
+    fn nondet_label() {
+        let data = Transition {
+            assignments: Vec::default(),
+            target: None,
+        };
+        let guarded_transition = GuardedTransition::Nondeterministic(&data);
+        let map = VariableMap::mock(Default::default());
+        assert_eq!(guarded_transition.label(&map), "");
+    }
+
+    #[test]
+    fn unguarded_label() {
+        let data = Transition {
+            assignments: Vec::default(),
+            target: None,
+        };
+        let guarded_transition = GuardedTransition::Unguarded(&data);
+        let map = VariableMap::mock(Default::default());
+        assert_eq!(guarded_transition.label(&map), "");
     }
 }
