cb09f6a5bd3136a30e4396fc32e1529b4253f58d
Radoslav Kvasňovský <rado.kvasnovsky@gmail.com>
2023-09-16 12:27:19 +0200

    Added DisplayLabel for Assignment

--- src/pts/transition.rs
+++ src/pts/transition.rs
@@ -3,6 +3,8 @@ use linear_polynomial::LinearPolynomial;
 use location::LocationHandle;
 use variable_map::Variable;
 
+use super::{variable_map::VariableMap, DisplayLabel};
+
 #[cfg_attr(test, derive(PartialEq))]
 #[derive(Debug)]
 #[repr(align(32))] // 32 bytes
@@ -14,6 +16,44 @@ pub struct Assignment(pub Variable, pub LinearPolynomial);
 pub struct Transition {
     pub assignments: Vec<Assignment>,
     pub target: LocationHandle,
+}
+
+impl DisplayLabel for Assignment {
+    fn label(&self, variable_map: &VariableMap) -> String {
+        let mut label = self.0.to_string();
+        label.push_str(" = ");
+        label.push_str(self.1.label(variable_map).as_str());
+
+        label
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pts::{
+        linear_polynomial::{constant::Constant, LinearPolynomial},
+        variable_map::{Variable, VariableMap},
+        DisplayLabel,
+    };
+
+    use super::Assignment;
+
+    #[test]
+    fn label() {
+        let assign = Assignment(
+            Variable::new("test"),
+            LinearPolynomial::mock(vec![Constant(0.0)]),
+        );
+        let map = VariableMap::mock(Default::default());
+        assert_eq!(assign.label(&map), "test = 0");
+
+        let assign = Assignment(
+            Variable::new("b"),
+            LinearPolynomial::mock(vec![Constant(-5.0), Constant(-2.0), Constant(1.0)]),
+        );
+        let map = VariableMap::mock(vec![Variable::new("a"), Variable::new("b")]);
+        assert_eq!(assign.label(&map), "b = -2a + b - 5");
+    }
 }
 
 // #[cfg(test)]
